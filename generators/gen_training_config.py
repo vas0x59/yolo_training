3f4b30cd5fc9c5b9a0b7c5204e95a56bfa047e57
vas59 <vasily.yuryev.a1@gmail.com>
2019-05-21 22:25:45 +0300

    done

--- generators/gen_training_config.py
+++ generators/gen_training_config.py
@@ -12,6 +12,10 @@ parser.add_argument("-t",
                     "--datasetPaths",
                     help="Path to the folder where the input .xml files are stored",
                     type=str)
+parser.add_argument("-bs",
+                    "--bathSize",
+                    help="Path to the folder where the input .xml files are stored",
+                    type=int)
 parser.add_argument("-b",
                     "--backupDir",
                     help="Path to the folder where the input .xml files are stored",
@@ -20,14 +24,45 @@ parser.add_argument("-o",
                     "--outputFile",
                     help="Path to the folder where the input .xml files are stored",
                     type=str)
+parser.add_argument("-cf",
+                    "--cfgFile",
+                    help="Path to the folder where the input .xml files are stored",
+                    type=str)
+parser.add_argument("-co",
+                    "--cfgFileOut",
+                    help="Path to the folder where the input .xml files are stored",
+                    type=str)
 args = parser.parse_args()
 
 file_classes = open(args.classes, "r")
-CLASSES = file_classes.read().split('\n')
+CLASSES = file_classes.read().split('\n')[:-1]
 
 file_out = open(args.outputFile, 'w')
 file_out.write("classes = " + str(len(CLASSES)) + "\n")
+# print(CLASSES)
 file_out.write("train = " + args.datasetPaths + '/train.txt' + "\n")
 file_out.write("test = " + args.datasetPaths + '/test.txt' + "\n")
 file_out.write("names = " + args.classes + "\n")
 file_out.write("backup = " + args.backupDir+'/' + "\n")
+
+cfg_file = open(args.cfgFile, 'r')
+cfg = cfg_file.readlines()
+
+cfg[2] = "batch=" + str(args.bathSize) + "\n"
+cfg[3] = "subdivisions=" + str(24) + "\n"
+
+cfg[126] = "filters=" + str((len(CLASSES) + 5)*3) + "\n"
+cfg[170] = "filters=" + str((len(CLASSES) + 5)*3) + "\n"
+
+cfg[134] = "classes=" + str(len(CLASSES)) + "\n"
+cfg[176] = "classes=" + str(len(CLASSES)) + "\n"
+
+cfg_out = open(args.cfgFileOut, 'w+')
+cfg_out.writelines(cfg)
+# cfg_str = ""
+# for i in cfg:
+#     cfg_str+=i
+
+# cfg_str = cfg_str.replace("batch=1", "batch="+str(args.bathSize))
+# cfg_str = cfg_str.replace("classes=80", "classes="+str(len(CLASSES)))
+# cfg = cfg.replace("classes=80", "classes="+str(len(CLASSES)))
--- test.sh
+++ test.sh
@@ -6,10 +6,12 @@ echo "Hello world"
 in_dir="/home/vasily/Projects/datasets/arduino_raspberry_metro/yolo"
 classes="/home/vasily/Projects/datasets/arduino_raspberry_metro/names.names"
 work_dir="./work"
+cfg="./cfgs/yolov3_tiny.cfg"
+bs=48
 per=10
 
 echo "gen_img_train_test"
 python ./generators/gen_img_train_test.py -i $in_dir -p $per -o $work_dir
 
 echo "gen_training_config"
-python ./generators/gen_training_config.py -c $classes -t $work_dir -b $work_dir"/backup" -o $work_dir"/obj.data"
+python ./generators/gen_training_config.py -c $classes -t $work_dir -b $work_dir"/backup" -o $work_dir"/obj.data" -bs $bs -cf $cfg -co $work_dir"/yolov3_tiny_cfg.cfg"
