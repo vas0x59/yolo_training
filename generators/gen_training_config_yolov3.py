2c4e38ac2972ab4824026e495757baa79fdd07ff
vas59 <vasily.yuryev.a1@gmail.com>
2019-05-24 06:51:33 +0300

    full yolov3 config generator

---
+++ generators/gen_training_config_yolov3.py
@@ -0,0 +1,70 @@
+import argparse
+
+import glob
+import os
+parser = argparse.ArgumentParser(
+    description="Sample TensorFlow XML-to-CSV converter")
+parser.add_argument("-c",
+                    "--classes",
+                    help="",
+                    type=str)
+parser.add_argument("-t",
+                    "--datasetPaths",
+                    help="Path to the folder where the input .xml files are stored",
+                    type=str)
+parser.add_argument("-bs",
+                    "--bathSize",
+                    help="Path to the folder where the input .xml files are stored",
+                    type=int)
+parser.add_argument("-b",
+                    "--backupDir",
+                    help="Path to the folder where the input .xml files are stored",
+                    type=str)
+parser.add_argument("-o",
+                    "--outputFile",
+                    help="Path to the folder where the input .xml files are stored",
+                    type=str)
+parser.add_argument("-cf",
+                    "--cfgFile",
+                    help="Path to the folder where the input .xml files are stored",
+                    type=str)
+parser.add_argument("-co",
+                    "--cfgFileOut",
+                    help="Path to the folder where the input .xml files are stored",
+                    type=str)
+args = parser.parse_args()
+
+file_classes = open(args.classes, "r")
+CLASSES = file_classes.read().split('\n')[:-1]
+
+file_out = open(args.outputFile, 'w')
+file_out.write("classes = " + str(len(CLASSES)) + "\n")
+# print(CLASSES)
+file_out.write("train = " + args.datasetPaths + '/train.txt' + "\n")
+file_out.write("test = " + args.datasetPaths + '/test.txt' + "\n")
+file_out.write("names = " + args.classes + "\n")
+file_out.write("backup = " + args.backupDir+'/' + "\n")
+
+cfg_file = open(args.cfgFile, 'r')
+cfg = cfg_file.readlines()
+
+cfg[2] = "batch=" + str(args.bathSize) + "\n"
+cfg[3] = "subdivisions=" + str(8) + "\n"
+
+cfg[602] = "filters=" + str((len(CLASSES) + 5)*3) + "\n"
+cfg[688] = "filters=" + str((len(CLASSES) + 5)*3) + "\n"
+cfg[775] = "filters=" + str((len(CLASSES) + 5)*3) + "\n"
+
+cfg[609] = "classes=" + str(len(CLASSES)) + "\n"
+cfg[695] = "classes=" + str(len(CLASSES)) + "\n"
+cfg[782] = "classes=" + str(len(CLASSES)) + "\n"
+
+cfg_out = open(args.cfgFileOut, 'w+')
+cfg_out.writelines(cfg)
+# cfg_str = ""
+# for i in cfg:
+#     cfg_str+=i
+
+# cfg_str = cfg_str.replace("batch=1", "batch="+str(args.bathSize))
+# cfg_str = cfg_str.replace("classes=80", "classes="+str(len(CLASSES)))
+# cfg = cfg.replace("classes=80", "classes="+str(len(CLASSES)))
